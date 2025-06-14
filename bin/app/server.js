351ab13e265da9d9e960aa9102881b2386638ae0
Shidqi Yusri <shidqifadhlu@gmail.com>
2025-06-14 13:33:07 +0700

    chore: setup redis

--- bin/app/server.js
+++ bin/app/server.js
@@ -9,7 +9,10 @@ const morgan = require("morgan");
 const helmet = require('helmet');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
+const redis = require('../config/redis');
+const logger = require('../helpers/utils/logger');
 const swaggerDocument = YAML.load(path.resolve(__dirname, '../docs/swagger.yaml'));
+const db = require('../models');
 
 class AppServer {
   constructor() {
@@ -22,6 +25,22 @@ class AppServer {
     // this.server.use(helmet()); //enable when in prod
 
     this.server.use(morgan(':method: :url :status :response-time ms - :res[content-length]', { stream: morganStream }));
+
+    // Check PostgreSQL connection
+    db.sequelize.authenticate()
+      .then(() => {
+        logger.info('PostgreSQL (Sequelize) is connected');
+      })
+      .catch((err) => {
+        logger.error('PostgreSQL connection failed:', err.message || err);
+      });
+
+    // Check redis connection
+    redis.ping().then(() => {
+      logger.info('Redis is ready');
+    }).catch((err) => {
+      logger.error('Redis not ready:', err);
+    });
 
     this.server.get('/', (req, res) => {
       wrapper.response(res, 'success', wrapper.data('Takua API'), 'This services is running properly.');
