d15dccd90175b21281c7ba03d7049d03d7ad5ee1
Shidqi Yusri <shidqifadhlu@gmail.com>
2025-06-14 23:42:52 +0700

    chore: add port config in sequelize

--- bin/config/config.js
+++ bin/config/config.js
@@ -4,19 +4,23 @@
 
 require('dotenv').config();
 
+const port = process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : undefined;
+
 module.exports = {
   development: {
     username: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || null,
     database: process.env.DB_NAME || 'mydb',
     host: process.env.DB_HOST || '127.0.0.1',
-    dialect: 'postgres'
+    port: port,
+    dialect: 'postgres',
   },
   test: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
+    port: port,
     dialect: 'postgres'
   },
   production: {
@@ -24,6 +28,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
+    port: port,
     dialect: 'postgres'
   }
 };
