8c7c2639a73ec28a518f199f8462bd27423540ed
Shidqi Yusri <shidqifadhlu@gmail.com>
2025-06-26 17:01:17 +0700

    fix: some issueagain

--- bin/controllers/transaction/transactionController.js
+++ bin/controllers/transaction/transactionController.js
@@ -128,7 +128,7 @@ module.exports.setTransferSchedule = async (req, res) => {
 
 module.exports.getTransferSchedule = async (req, res) => {
   const userData = req.userData;
-  const pocket_id = req.params.pocket_id;
+  const pocket_id = req.params.pocketId;
 
   transferModules.getTransferSchedule(userData, pocket_id)
     .then(resp => {
