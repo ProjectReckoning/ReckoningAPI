8a1aa01d8a711f6b8815e0615e83fdae2bd943a2
Shidqi Yusri <shidqifadhlu@gmail.com>
2025-07-07 10:18:15 +0700

    fix: add log dir path

--- bin/helpers/utils/logger.js
+++ bin/helpers/utils/logger.js
@@ -1,5 +1,13 @@
 const { createLogger, transports, format } = require('winston');
 const DailyRotateFile = require('winston-daily-rotate-file');
+const fs = require('fs');
+const path = require('path');
+
+// Ensure the logs directory exists
+const logDir = path.join(__dirname, '../../../logs'); // Adjust based on your file's depth
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir, { recursive: true });
+}
 
 const logger = createLogger({
   level: 'info',
@@ -7,7 +15,7 @@ const logger = createLogger({
     new transports.Console(),
     // new transports.File({ filename: 'app.log' }) // optional
     new DailyRotateFile({
-      filename: 'logs/reckoningAPI-%DATE%.log',
+      filename: path.join(logDir, 'reckoningAPI-%DATE%.log'),
       datePattern: 'YYYY-MM-DD',
       zippedArchive: false,
       maxSize: '20m',
